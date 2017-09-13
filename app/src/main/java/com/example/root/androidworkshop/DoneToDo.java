192c0f10a9ec64618ace1f343c5fbe1513f790ae
Ng Wei Shyang <31458338+gaara4896@users.noreply.github.com>
2017-09-13 11:28:54 +0800

    Revert "Revert "Simplified""

--- app/src/main/java/com/example/root/androidworkshop/DoneToDo.java
+++ app/src/main/java/com/example/root/androidworkshop/DoneToDo.java
@@ -2,11 +2,12 @@ package com.example.root.androidworkshop;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Vector;
 
 public class DoneToDo extends AppCompatActivity {
 
@@ -18,24 +19,25 @@ public class DoneToDo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_done_to_do);
 
+        //get back arrow to parent activity
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
         //Link gui component to object
         listView_DoneToDo = (ListView)findViewById(R.id.listView_DoneToDo);
         db = new SQLiteManager(getApplicationContext());
 
         //get the Done to do list from SQLite
-        Vector<ToDo> vec = db.getDoneToDo();
-        ToDo[] doneToDo = new ToDo[vec.size()];
+        ArrayList<ToDo> listArray = db.getDoneToDo();
 
-        //convert from vector to object array
-        int counter = 0;
-        Iterator i = vec.iterator();
-        while(i.hasNext()){
-            doneToDo[counter] = (ToDo) i.next();
-            counter++;
+
+        final String[] toDoArray = new String[listArray.size()];
+        for(int i=0;i<listArray.size();i++)
+        {
+            toDoArray[i]=listArray.get(i).getName();
         }
 
         //Set the listview to the done to do list
-        ListAdapter adapter = new ListViewFragment(this, doneToDo);
-        listView_DoneToDo.setAdapter(adapter);
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,toDoArray);
+        listView_DoneToDo.setAdapter(listAdapter);
     }
 }
